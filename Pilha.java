ef08d91c8b887ae13db26bc1002507eefb583627
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-08-20 11:43:56 -0300

    Update Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -1,19 +1,37 @@
-import java.util.Stack;
+public class Pilha {
+    private int top;
+    private int[] data;
 
-public class Pilha extends Stack {
-    private int top =-1;
-    private int [] data;
-
-    public Pilha(int capacidade){
-        int capa = capacidade;
+    public Pilha() {
+        data = new int[4];
+        top = -1;
     }
 
-    public void push (int data){
-        top = top + 1;
+    public boolean isVazia() {
+        return top == -1;
     }
 
-    public int pop(int data){
-        top = top - 1;
-        return top;
+    public void push(int elemento) {
+        if (top == data.length - 1) {
+            redimensionar(2 * data.length);
+        }
+        data[++top] = elemento;
     }
-}
+
+    private void redimensionar(int novaCapacidade) {
+        int[] novaPilha = new int[novaCapacidade];
+        for (int i = 0; i <= top; i++) {
+            novaPilha[i] = data[i];
+        }
+        data = novaPilha;
+    }
+
+    public int pop() {
+        if (isVazia()) {
+            System.out.println("Pilha vazia. Não há elementos para desempilhar.");
+            return -1;
+        }
+        int elementoRemovido = data[top--];
+
+    return elementoRemovido;
+}}
